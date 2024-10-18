0985f651b97e7e33757d7b6cdbf356e0c44c826e
Neeraj Pandey <106172268+neerajpandit@users.noreply.github.com>
2024-10-18 13:17:39 +0530

    Update Career.jsx

--- src/component/Careers/Career.jsx
+++ src/component/Careers/Career.jsx
@@ -56,7 +56,6 @@ const Career = () => {
                                         </button>
                                     </a>
                                 </div>
-
                             </div>
                         </div>
                     </div>
